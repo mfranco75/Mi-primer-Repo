05409f9f550f4e2c9e6f8ab9d5037fd0572ffa2b
MarianoFranco <marianofranco1975@gmail.com>
2024-09-21 10:48:06 -0300

    Hello Git con descripciones

--- HelloGit1.py
+++ HelloGit1.py
@@ -1,2 +1,9 @@
-print("Hello Git v2")
+print("Hello Git v2.1")
+# para hacer commit :
+# git add .  (agrega todos los archivos y cambios)
+# git commit -m "descripcion del commit"
+# git checkout iddelcomit (se va a un punto d commit guardado específico)
+# git status (ver estatus de cambios)
+# git branch nombrederama (crea una rama de trabajo paralela)
+# git branch switch nombredelarama (cambia a la rama)
 
