23fcb7b68692eb263478b32dfd891184f99f4f27
MarianoFranco <marianofranco1975@gmail.com>
2024-09-21 10:59:23 -0300

    rama de implementacion login

--- HelloGitHub1.py
+++ HelloGitHub1.py
@@ -1,1 +1,2 @@
 print("Hello GitHub v1")
+#se crea clave publica y privada con HSSgit
---
+++ login.py
@@ -0,0 +1,12 @@
+print("login")
+#
+# PS C:\Programacion\HelloGit> git branch login (crea rama login)
+
+#PS C:\Programacion\HelloGit> git switch login (cambia a la rama login)
+#Switched to branch 'login'
+#PS C:\Programacion\HelloGit> git tree        
+#* 05409f9 (HEAD -> login) Hello Git con descripciones
+#* f425e9b se agrega git ignore
+#* 724001a GitHub v1
+#* ce49d8b (main) Mi primer commit
+##
